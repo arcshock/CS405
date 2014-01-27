31d3153cc8412e8d56d1bf6dbfc98821a8d0b102
arcshock <frozenouthere@gmail.com>
2014-01-27 12:46:14 -0900

    Constants for nn added

--- CS405/Homeworks/Homework1/neuralnet.h
+++ CS405/Homeworks/Homework1/neuralnet.h
@@ -6,4 +6,9 @@
  * Date: 2014-01-27
  */
 
+const unsigned int INPUT_NODE_NUM = 32;
+const unsigned int HID_L0_NODE_NUM = 40;
+const unsigned int HID_L1_NODE_NUM = 10;
+const unsigned int OUTPUT_NODE_NUM = 1;
+
 
