0e7127982f5370ed5dddd77a6ccd584e35fb82eb
voot <frozenouthere@gmail.com>
2014-02-05 10:01:40 -0900

    Trying to get iterators passed to the constructor of class

--- CS405/Homeworks/Homework1/includes/neuralnet.h
+++ CS405/Homeworks/Homework1/includes/neuralnet.h
@@ -7,8 +7,26 @@
  * Purpose: Header of neural network class.
  */
 
-class NeuralNet{
+#include<array>
+#include<ostream>
+#include<cstdlib>
+
+template<typename BidirectionalIterator>
+
+class NeuralNet {
 public:
+//	NeuralNet(int input, std::array<int, SIZE>& layers, int output);
+	NeuralNet(int input, BidirectionalIterator begin, BidirectionalIterator end, int output);
+
 private:
+//	friend std::ostream& operator<< <SIZE>(std::ostream&, const NeuralNet<SIZE> &);
+	int j = 6;
+	
 };
+
+/*
+template<std::size_t SIZE>
+std::ostream& operator<<(std::ostream &stream, const NeuralNet<SIZE> & net) {
+	return stream << "Test(" << net.j << ")";
+}*/
 #endif //NEURAL_NET_H
--- CS405/Homeworks/Homework1/src/main.cpp
+++ CS405/Homeworks/Homework1/src/main.cpp
@@ -1,7 +1,10 @@
 #include<neuralnet.h>
 #include<iostream>
+#include<array>
 
 int main()
 {
+	std::array<int, 2> hidden_layers = {{40, 10}};
+	NeuralNet <int> test;// = NeuralNet <int>(32, hidden_layers.begin, hidden_layers.end, 1);
 	return 0;
 }
