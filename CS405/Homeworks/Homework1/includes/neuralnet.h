d644e55a683877872c2a33d649738f2503073cc7
arcshock <frozenouthere@gmail.com>
2014-02-04 18:16:03 -0900

    changed class name to NeuralNet, cleaned up the const variables of the test NN arch

--- CS405/Homeworks/Homework1/includes/neuralnet.h
+++ CS405/Homeworks/Homework1/includes/neuralnet.h
@@ -5,19 +5,10 @@
  * File: neuralnet.h
  * CS 405 Intro to A.I.
  * Purpose: Header of neural network class.
- * Date: 2014-01-27
  */
 
-const unsigned int INPUT_NODE_NUM = 32;
-const unsigned int HID_L0_NODE_NUM = 40;
-const unsigned int HID_L1_NODE_NUM = 10;
-const unsigned int OUTPUT_NODE_NUM = 1;
-
-class InputNeuralNode {
+class NerualNet{
 public:
-	void setBias(double);
 private:
-	double m_outputVal;
-	double m_input;
 };
 #endif //NEURAL_NET_H
