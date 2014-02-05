c791a3596b8c2e2329ac0584ab61e4ba1f6c9638
arcshock <frozenouthere@gmail.com>
2014-02-05 07:38:07 -0900

    Added a func to print network and constructor

--- CS405/Homeworks/Homework1/includes/neuralnet.h
+++ CS405/Homeworks/Homework1/includes/neuralnet.h
@@ -7,32 +7,59 @@
  * Purpose: Header of neural network class.
  */
 
+#include<vector>
 #include<array>
-#include<ostream>
+//#include<ostream>
 #include<iterator>
 #include<cstdlib>
+#include<iostream>
 
-//template<typename T>
 template<class Iter>
 class NeuralNet {
 public:
 	NeuralNet();
 	NeuralNet(int input, Iter begin, Iter end, int output);
-	NeuralNet(Iter begin, Iter end);
-//	NeuralNet(int input, int* begin, int* end, int output);
+	NeuralNet(Iter begin, Iter end, int size);
+	void setWeights();
 
 private:
-	
-	
+	void printNetwork();
+	std::vector< std::vector< double > > network_m;
+	//TODO: pairs for bias and weight of each node
 };
 
 template<class Iter>
-NeuralNet<Iter>::NeuralNet (Iter begin, Iter output)
+void NeuralNet<Iter>::printNetwork()
 {
-	//TODO
+	for ( auto iter = network_m.begin(); iter != network_m.end(); ++iter ) {
+		for ( auto iterr = iter->begin(); iterr != iter->end(); ++iterr ) {
+			std::cout << *iterr << std::endl;
+		}
+	}
+}
+
+template<class Iter>
+NeuralNet<Iter>::NeuralNet (Iter begin, Iter end, int size )
+{
+	Iter it = begin;
+	network_m.reserve( size );
+	for (int i = 0; i < size; ++i) {
+		network_m[i].reserve(*it);
+		++it;
+	}
+
+/*
+	network_m.reserve(size);
+	std::vector< std::vector< double >>::iterator network_iter = network_m.begin();
+
+	for ( auto it = begin; it != end; it++ ) {
+//		std::cout << *it << std::endl; //debug output
+		network_iter[].reserve( *it );
+	}
+*/
 }
 template<class Iter>
-NeuralNet<Iter>::NeuralNet(int input, Iter begin, Iter end, int output)
+NeuralNet<Iter>::NeuralNet (int input, Iter begin, Iter end, int output)
 {
 	//TODO
 }
