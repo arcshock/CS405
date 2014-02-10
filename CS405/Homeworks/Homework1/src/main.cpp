577add487ec2c2bcf84671650f24995693e1f4c0
Bucky Frost <frozenouthere@gmail.com>
2014-02-10 12:29:46 -0900

    Cleaned up some of the old code that was in main and some in the timing functions.

--- CS405/Homeworks/Homework1/src/main.cpp
+++ CS405/Homeworks/Homework1/src/main.cpp
@@ -11,7 +11,6 @@
 #include<ctime>
 #include<string>
 #include <sstream>
-using std::ostringstream;
 
 std::string timingManager(int iterations, int numOfBoardEval, NeuralNet<std::array<int, 4>::iterator> & neuralNet);
 double timingFunc(int iterations, NeuralNet<std::array<int, 4>::iterator> & neuralNet); // used for timing board evals
@@ -28,23 +27,6 @@ int main()
 
 	std::cout << timingManager(20, 11500, test1) << std::endl;
 
-/*	std::vector<double> times;
-	double totalTime = 0;
-
-	for (int i = 0; i < 20; ++i) {
-		times.push_back(timingFunc(11500, test1));
-		std::cout << "Evaluating new board " << i << std::endl; // debug
-	}
-
-	for (auto i : times) {
-//		std::cout << i << std::endl; // debug
-		totalTime += i;	
-	}
-//	std::cout << "totalTime:" << totalTime << std::endl; // debug
-
-	std::cout << "Finished 11500 evalualuations for 20 boards in an average time of " << totalTime/20 << " seconds." << std::endl;
-	std::cout << "There are  " << 11500/(totalTime/20) << " evauations per second." << std::endl;
-//	timingFunc( 11500, test1 ); */
 	return 0;
 }
 
@@ -77,7 +59,7 @@ std::string timingManager(int iterations, int numOfBoardEval, NeuralNet<std::arr
 		std::cout << "Evaluating new board " << i << std::endl;
 	}
 
-	ostringstream timingMessage;
+	std::ostringstream timingMessage;
 	timingMessage << "Completed " << iterations << " iterations of " << numOfBoardEval << " board evaluations in average time of " << timeavg(times.begin(), times.end(), iterations) << " seconds.";
 
 	return timingMessage.str();
@@ -87,11 +69,8 @@ double timeavg(std::vector<double>::iterator begin, std::vector<double>::iterato
 {	
 	double runningTotal = 0;
 
-	for (auto iter = begin; begin != end; ++begin) {
+	for (auto iter = begin; begin != end; ++begin)
 		runningTotal += *iter;
-	}
-	std::cout << "Running Total = " << runningTotal << std::endl;
-	std::cout << "Number of samples = " << samples << std::endl;
-	std::cout << "Average = " << runningTotal/samples << std::endl;
+
 	return runningTotal/samples;
 }
