885eb68363ddf2ea8b8e84032f760f454b37c7da
Bucky Frost <frozenouthere@gmail.com>
2014-02-18 08:53:40 -0900

    Switched the template from the iterator to the size of the input array.

--- CS405/Homeworks/Homework2/test/feedforward.cpp
+++ CS405/Homeworks/Homework2/test/feedforward.cpp
@@ -1,19 +1,23 @@
 /*
  * Author: Bucky Frost
- * File: feedforward.cpp
- * Purpose: Just calling init on the network and feedforward function
- * 	for profiling purposes.
+ * File: main.cpp
+ * Purpose: Main function for managing the neural network.
  * CS 405 Intro to A.I.
  */
-#include "neuralnet.h"
-#include "node.h"
-#include <array>
+#include"neuralnet.h"
+#include<iostream>
+#include<array>
+#include<chrono>
+#include<ctime>
+#include<string>
+#include <sstream>
+
 
 int main()
 {
 	std::array<int, 4> hidden_layers = {{ 32, 40, 10, 1 }};
 
-	NeuralNet < std::array<int, 4>::iterator > test1 ( hidden_layers.begin(), hidden_layers.end(), hidden_layers.size());
+	NeuralNet < hidden_layers.size() > test1 (hidden_layers);
 	test1.loadInput();
 	test1.setWeights();
 	test1.forwardFeed();
