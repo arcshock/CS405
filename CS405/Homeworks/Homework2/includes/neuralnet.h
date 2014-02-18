c80089e0b6645ae1fe34a988bc75063e61eabe8b
Bucky Frost <frozenouthere@gmail.com>
2014-02-18 02:26:20 -0900

    Replace Class tempating of iterators with size of the std::array.

--- CS405/Homeworks/Homework2/includes/neuralnet.h
+++ CS405/Homeworks/Homework2/includes/neuralnet.h
@@ -8,19 +8,20 @@
 #ifndef NEURAL_NET_H
 #define NEURAL_NET_H 
 
-#include"node.h"
-#include<vector>
-#include<array>
-#include<iterator>
-#include<cstdlib>
-#include<iostream>
-#include<random>
+#include "node.h"
+#include <vector>
+#include <iterator>
+#include <cstdlib>
+#include <iostream>
+#include <array>
 
-template<class Iter>
+template<std::size_t SIZE>
 class NeuralNet {
 public:
-	NeuralNet(int input, Iter begin, Iter end, int output);
-	NeuralNet(Iter begin, Iter end, int size);
+
+//	NeuralNet(int input, SIZE begin, SIZE end, int output);
+//	NeuralNet(SIZE begin, SIZE end, int size);
+	NeuralNet(std::array<int, SIZE>& arr);
 	void setWeights();
 	void printNetwork();
 	void loadInput();
@@ -35,35 +36,34 @@ private:
 	std::vector< std::vector<Node> > network_m;
 };
 
-template<class Iter>
-double NeuralNet<Iter>::evalFunc()
+template<std::size_t SIZE>
+double NeuralNet<SIZE>::evalFunc()
 {
 	evalOutput_m = (1 - network_m.back().back().getOutput()); // TODO multiply the sigmoid func
 	return evalOutput_m;
 }
 
-template<class Iter>
-void NeuralNet<Iter>::loadInput()
+template<std::size_t SIZE>
+void NeuralNet<SIZE>::loadInput()
 {
 	for ( int j = 0; j < network_m[0].size(); ++j ) {
 		network_m[0][j].setOutput(network_m[0][j].randomValue());
 	}
 }
 
-template<class Iter>
-NeuralNet<Iter>::NeuralNet (Iter begin, Iter end, int numOfLayers)
+template<std::size_t SIZE>
+NeuralNet<SIZE>::NeuralNet (std::array<int, SIZE>& arr)
 {
-	Iter layerSize = begin;
-	network_m.reserve(numOfLayers);
+	auto numberOfLayers = arr.size();
+	network_m.reserve(numberOfLayers);
 
-	for (int i = 0; i < numOfLayers; ++i) {     //creates each layer
-		network_m.push_back(std::vector<Node>(*layerSize));
-		++layerSize;
+	for (int i = 0; i < numberOfLayers; ++i){
+		network_m.push_back(std::vector<Node>(arr[i]));
 	}
 }
 
-template<class Iter>
-void NeuralNet<Iter>::setWeights()
+template<std::size_t SIZE>
+void NeuralNet<SIZE>::setWeights()
 {
 	for (int i = 0; i < network_m.size(); ++i) {
 		for (int j = 0; j < network_m[i].size(); ++j) {
@@ -72,8 +72,8 @@ void NeuralNet<Iter>::setWeights()
 	}
 }
 
-template<class Iter>
-void NeuralNet<Iter>::mutateNetwork()
+template<std::size_t SIZE>
+void NeuralNet<SIZE>::mutateNetwork()
 {
 	for (int i = 1; i < network_m.size(); ++i) { // staring at hidden layer
 		for ( int j = 0; j < network_m[i].size(); ++j) {
@@ -82,8 +82,8 @@ void NeuralNet<Iter>::mutateNetwork()
 	}
 }
 
-template<class Iter>
-void NeuralNet<Iter>::printNetwork()
+template<std::size_t SIZE>
+void NeuralNet<SIZE>::printNetwork()
 {
 	for (int i = 1; i < network_m.size(); ++i) {
 		std::cout << "Printing layer " << i << std::endl;
@@ -94,8 +94,8 @@ void NeuralNet<Iter>::printNetwork()
 	}
 }
 
-template<class Iter>
-void NeuralNet<Iter>::forwardFeed()
+template<std::size_t SIZE>
+void NeuralNet<SIZE>::forwardFeed()
 {
 	for (int i = 1; i < network_m.size(); ++i) { //starting at the first hidden layer
 		for ( int j = 0; j < network_m[i].size(); ++j ) { //chose node in hiddent layer and apply all of last layer to it
