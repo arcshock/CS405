2405f7be8cde395f9827da3045fefce28a4f2a66
Bucky Frost <frozenouthere@gmail.com>
2014-02-25 10:00:00 -0900

    Added two helper functions for the board printing, one for the caps of cells and the other for printing the spaces.

--- CS405/Homeworks/Homework3/src/board.cpp
+++ CS405/Homeworks/Homework3/src/board.cpp
@@ -4,12 +4,45 @@
  * Purpose: Implementation file for checkers board representation.
  */
 
+#include "board.h"
 /* == Function printBoard ==================
  * 
  * Precondition: None.
  *
  * Post Condition: Prints out a checkers board rep to std out.
  * ========================================= */
+
+void printCellTop()
+{
+	for (auto i = 0; i < 8; ++i) {
+		std::cout << "+";
+
+		for (auto j = 0; j < 5; ++j) {
+			std::cout << "-";
+		}
+	}
+	std::cout << "+" << std::endl;
+}
+
+void printCellSpace()
+{
+	for (auto i = 0; i < 8; ++i) {
+		std::cout << "|";
+
+		for (auto j = 0; j < 5; ++j) {
+			std::cout << " ";
+		}
+	}
+	std::cout << "|" << std::endl;
+}
+
 void printBoard()
 {
+	auto boardHeight = 8;
+
+	for (auto i = 0; i < boardHeight; ++i) {
+		printCellTop();
+		printCellSpace();
+	}
+	printCellTop();
 }
