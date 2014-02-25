dc3d677813b828686dd2fb040f396ccb61ad657e
Bucky Frost <frozenouthere@gmail.com>
2014-02-25 09:10:16 -0900

    Added file preamble, and start of the printBoard function.

---
+++ CS405/Homeworks/Homework3/src/board.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: Bucky Frost
+ * File: board.cpp
+ * Purpose: Implementation file for checkers board representation.
+ */
+
+/* == Function printBoard ==================
+ * 
+ * Precondition: None.
+ *
+ * Post Condition: Prints out a checkers board rep to std out.
+ * ========================================= */
+void printBoard()
+{
+}
