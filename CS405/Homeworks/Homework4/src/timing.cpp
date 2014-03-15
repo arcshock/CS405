10738f788fae9d57753ef3a64cc41c728b67a3ef
Bucky Frost <frozenouthere@gmail.com>
2014-03-15 14:50:20 -0800

    Removed the timing function definitions from the implementation file
    timing.cpp.

--- CS405/Homeworks/Homework4/src/timing.cpp
+++ CS405/Homeworks/Homework4/src/timing.cpp
@@ -5,17 +5,13 @@
  * CS 405 Intro to A.I.
  */
 #include "neuralnet.h"
+#include "timing.h"
 #include <iostream>
 #include <array>
 #include <chrono>
 #include <string>
 #include <future>
 #include <functional> 
-
-std::string timingManager(NeuralNet<4> & neuralNet, int iterations);
-double timingFunc(int iterations, NeuralNet<4> & neuralNet); // used for timing board evals
-double timeavg(std::vector<double>::iterator begin, std::vector<double>::iterator end, int samples);
-void messaging(std::string message);
 
 // Precondition:
 // Message is a string value to inform the user.
