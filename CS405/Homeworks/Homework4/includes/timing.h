42aebbab951b4ce16ec1ee7d11332815f4192d82
Bucky Frost <frozenouthere@gmail.com>
2014-03-15 11:44:00 -0800

    Added the header file for the timing function.

---
+++ CS405/Homeworks/Homework4/includes/timing.h
@@ -0,0 +1,24 @@
+/*
+ * Author: Bucky Frost
+ * File: timing.cpp
+ * Purpose: Header file for the timing functionalilty
+ * CS 405 Intro to A.I.
+ */
+
+#ifndef TIMING_HPP
+#define TIMING_HPP
+
+#include "neuralnet.h"
+#include <iostream>
+#include <array>
+#include <chrono>
+#include <string>
+#include <future>
+#include <functional> 
+
+std::string timingManager(NeuralNet<4> & neuralNet, int iterations);
+double timingFunc(int iterations, NeuralNet<4> & neuralNet); // used for timing board evals
+double timeavg(std::vector<double>::iterator begin, std::vector<double>::iterator end, int samples);
+void messaging(std::string message);
+
+#endif /*TIMING_HPP*/
