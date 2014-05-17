ee8b08b47822c8609ad17703289a5189cd959dca
Bucky Frost <frozenouthere@gmail.com>
2014-05-17 14:53:26 -0800

    Modified tests.cpp
    Initial test cases for the evaluation of the neural network class.

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,9 +7,26 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
-#include <string>
 #include "neural_network.hpp"
 
-TEST_CASE( "Initial Neural" ) {
-	REQUIRE( feed_forward() == 0 );
+TEST_CASE( "Initial Neural Network Class" ) {
+
+	Neural_Network first_net;
+
+	std::vector<double> negative_checker_board(32, -1.0);
+	double solution1 = (-1.0*32.0)/(1.0 + abs(-1.0*32.0));
+
+
+	std::vector<double> empty_checker_board(32, 0.0);
+	double solution2 = (0.0*32.0)/(1.0 + abs(0.0*32.0));
+
+
+	std::vector<double> full_checker_board(32, 1.0);
+	double solution3 = (1.0*32.0)/(1.0 + abs(1.0*32.0));
+
+	REQUIRE( first_net.network_evaluate(negative_checker_board) == solution1 );
+	REQUIRE( first_net.network_evaluate(empty_checker_board) == solution2 );
+	REQUIRE( first_net.network_evaluate(full_checker_board) == solution3 );
 }
+
+//TEST_CASE()
