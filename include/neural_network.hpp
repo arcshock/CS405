3f120048c58afad9deb932509fc20226efea1e4e
Bucky Frost <frozenouthere@gmail.com>
2014-05-17 14:51:31 -0800

    Modified neural_network.hpp
    Added the class structure for the neural network, include guards, and
    moved the sigmoid into its own function.

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -1,11 +1,33 @@
+#ifndef NEURAL_NETWORK_HPP
+#define NEURAL_NETWORK_HPP
 /*
  * Author: Bucky Frost
  * File: neural_network.hpp
  * Purpose: Header file for the neural network.
  */
 
+#include <vector>
 
-double feed_forward()
+class Neural_Network
 {
-	return 0;
-}
+public:
+	/*Using compiler generated silent 4*/
+
+	// Feed forward the network to evaluate the checker board.
+	double network_evaluate(std::vector<double> & board_input)
+	{
+		double evaluation_value = 0.0;
+
+		for ( auto iter : board_input ) {
+			evaluation_value += iter;
+		}
+
+		return sigmoid(evaluation_value);
+	}
+private:
+	double sigmoid(double input)
+	{
+		return input/(1.0 + abs(input));
+	}
+};
+#endif /*NEURAL_NETWORK_HPP*/
