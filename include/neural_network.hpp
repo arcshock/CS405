0f8be0fca3ff65927ebb06019e27719b3bbe82d2
Bucky Frost <frozenouthere@gmail.com>
2014-05-18 02:09:13 -0800

    Modified neural_network.hpp
    Added a constructor that takes a vector that defines the layout of the
    network. Will initialize a private member variable network_ to a 2D
    vector of layers and the elements of the second vector are the nodes.
    
    Currently, the network is simply just doubles.

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -7,11 +7,28 @@
  */
 
 #include <vector>
+#include <random>
+
+static std::random_device randomDevice;
+static std::mt19937 random_value(randomDevice());
+static std::uniform_real_distribution<> uniform_distribution(-1, 1);
+
 
 class Neural_Network
 {
 public:
-	/*Using compiler generated silent 4*/
+	Neural_Network() = default;
+
+
+	Neural_Network(std::vector<int> network_specifications)
+	{
+		int network_layers = network_specifications.size();
+
+		for (int ii = 0; ii < network_layers; ++ii) {
+			network_.emplace_back(std::vector<double>(network_specifications[ii], uniform_distribution(random_value)));
+		}
+	}
+
 
 	// Feed forward the network to evaluate the checker board.
 	double network_evaluate(std::vector<double> & board_input)
@@ -24,10 +41,15 @@ public:
 
 		return sigmoid(evaluation_value);
 	}
+
+
 private:
 	double sigmoid(double input)
 	{
 		return input/(1.0 + abs(input));
 	}
+
+
+	std::vector<std::vector<double>> network_;
 };
 #endif /*NEURAL_NETWORK_HPP*/
