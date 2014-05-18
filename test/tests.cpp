bbe356d9934fab8c2c8f850a64a235a7ae205f26
Bucky Frost <frozenouthere@gmail.com>
2014-05-18 13:54:23 -0800

    Modified tests.cpp
    Added in a test case for the use of serialization of the neural
    networks.

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,28 +9,24 @@
 #include "catch.hpp"
 #include "neural_network.hpp"
 
+
+std::vector<double> negative_checker_board(32, -1.0);
+std::vector<double> empty_checker_board(32, 0.0);
+std::vector<double> full_checker_board(32, 1.0);
+
+std::vector<int> standard_network = {32, 40, 10, 1};
+
 TEST_CASE( "Default Constructed Neural Network Class" ) {
 
 	Neural_Network first_net;
 
-	std::vector<double> negative_checker_board(32, -1.0);
 	double solution1 = (-1.0*32.0)/(1.0 + abs(-1.0*32.0));
-
-
-	std::vector<double> empty_checker_board(32, 0.0);
 	double solution2 = (0.0*32.0)/(1.0 + abs(0.0*32.0));
-
-
-	std::vector<double> full_checker_board(32, 1.0);
 	double solution3 = (1.0*32.0)/(1.0 + abs(1.0*32.0));
 
-/*	REQUIRE( first_net.network_evaluate(negative_checker_board) == solution1 );
-	REQUIRE( first_net.network_evaluate(empty_checker_board) == solution2 );
-	REQUIRE( first_net.network_evaluate(full_checker_board) == solution3 );*/
 }
 
 TEST_CASE( "Specified Network Construction" ) {
-	std::vector<double> empty_checker_board(32, 0.0);
 	std::vector<double> negative_checker_board(32, -1.0);
 
 	std::vector<int> network_layout = {32, 1, 1, 1};
@@ -39,4 +35,35 @@ TEST_CASE( "Specified Network Construction" ) {
 
 	REQUIRE( constructed_network.network_evaluate(empty_checker_board) != 0.0 );
 	REQUIRE( constructed_network.network_evaluate(negative_checker_board) != 0.0 );
+}
+
+TEST_CASE( "Network Operators" ) {
+	Neural_Network first_network(standard_network);
+	Neural_Network second_network(standard_network);
+
+	REQUIRE( first_network == first_network);
+
+}
+TEST_CASE( "Network File I/O" ) {
+	Neural_Network saved_network(standard_network);
+
+	std::ofstream ofs("test_network_save.txt");
+
+	boost::archive::text_oarchive oa(ofs);
+
+	oa << saved_network;
+	ofs.close();
+
+	saved_network.network_evaluate(negative_checker_board);
+
+	Neural_Network restored_network(standard_network);
+
+	std::ifstream ifs("test_network_save.txt");
+	boost::archive::text_iarchive ia(ifs);
+	ia >> restored_network;
+
+	REQUIRE( saved_network != restored_network );	
+
+	restored_network.network_evaluate(negative_checker_board);
+	REQUIRE( restored_network == saved_network );	
 }
