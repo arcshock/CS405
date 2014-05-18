2297bd2b0105fcbadebb14431c76ef26c3d5d4ca
Bucky Frost <frozenouthere@gmail.com>
2014-05-18 04:34:55 -0800

    Modified tests.cpp
    Attempted to write tests for the output of the evaluation function with
    randomization of the initial network construction.

--- test/tests.cpp
+++ test/tests.cpp
@@ -24,13 +24,19 @@ TEST_CASE( "Default Constructed Neural Network Class" ) {
 	std::vector<double> full_checker_board(32, 1.0);
 	double solution3 = (1.0*32.0)/(1.0 + abs(1.0*32.0));
 
-	REQUIRE( first_net.network_evaluate(negative_checker_board) == solution1 );
+/*	REQUIRE( first_net.network_evaluate(negative_checker_board) == solution1 );
 	REQUIRE( first_net.network_evaluate(empty_checker_board) == solution2 );
-	REQUIRE( first_net.network_evaluate(full_checker_board) == solution3 );
+	REQUIRE( first_net.network_evaluate(full_checker_board) == solution3 );*/
 }
 
 TEST_CASE( "Specified Network Construction" ) {
-	std::vector<int> network_layout = {1, 1, 1, 1};
+	std::vector<double> empty_checker_board(32, 0.0);
+	std::vector<double> negative_checker_board(32, -1.0);
 
-	Neural_Network first_constructed_network(network_layout);
+	std::vector<int> network_layout = {32, 1, 1, 1};
+
+	Neural_Network constructed_network(network_layout);
+
+	REQUIRE( constructed_network.network_evaluate(empty_checker_board) != 0.0 );
+	REQUIRE( constructed_network.network_evaluate(negative_checker_board) != 0.0 );
 }
