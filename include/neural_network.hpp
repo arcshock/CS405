8f413d962e68a7955aee9308217c982cd3af6c76
Bucky Frost <frozenouthere@gmail.com>
2014-05-18 04:08:24 -0800

    Modification of neural_network.hpp
    Added a struct for the nodes of the neural network and switched
    pre-existing functions to use it.

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -14,6 +14,24 @@ static std::mt19937 random_value(randomDevice());
 static std::uniform_real_distribution<> uniform_distribution(-1, 1);
 
 
+struct network_node
+{
+	double _input;
+	double _weight;
+
+	network_node()
+	{
+		_input = 1.0;
+		_weight = uniform_distribution(random_value);
+	}
+
+	double node_value()
+	{
+		return _input*_weight;
+	}
+};
+
+
 class Neural_Network
 {
 public:
@@ -25,7 +43,7 @@ public:
 		int network_layers = network_specifications.size();
 
 		for (int ii = 0; ii < network_layers; ++ii) {
-			network_.emplace_back(std::vector<double>(network_specifications[ii], uniform_distribution(random_value)));
+			_network.emplace_back(std::vector<network_node>(network_specifications[ii]));
 		}
 	}
 
@@ -35,10 +53,25 @@ public:
 	{
 		double evaluation_value = 0.0;
 
-		for ( auto iter : board_input ) {
-			evaluation_value += iter;
+		int network_input_size = _network[0].size();
+		int input_layer = 0;
+
+		if (network_input_size == board_input.size()) {
+			for (int ii = 0; ii < network_input_size; ++ii) {
+				_network[input_layer][ii]._input = board_input[ii];
+			}
+		} else {
+			exit(-1); // Input to eval function does not line up with the network
 		}
 
+		for (int network_layer = input_layer + 1; network_layer < _network.size(); ++network_layer) {
+			for (int layer_column = 0; layer_column < _network[network_layer].size(); ++layer_column) {
+				for (int ii = 0; ii < _network[network_layer - 1].size(); ++ii)
+					_network[network_layer][layer_column]._input += sigmoid(_network[network_layer - 1][ii].node_value());
+			}
+		}
+
+		evaluation_value = _network.back().back().node_value();
 		return sigmoid(evaluation_value);
 	}
 
@@ -48,8 +81,8 @@ private:
 	{
 		return input/(1.0 + abs(input));
 	}
+	
 
-
-	std::vector<std::vector<double>> network_;
+	std::vector<std::vector<network_node>> _network;
 };
 #endif /*NEURAL_NETWORK_HPP*/
