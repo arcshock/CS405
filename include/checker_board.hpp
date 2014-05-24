d120e18ffa121983f8c84ff8782ed750e276fbe4
Bucky Frost <frozenouthere@gmail.com>
2014-05-23 17:04:33 -0800

    Modified checker_board.hpp
    Added in the default constructor to deal with the committing the initial
    pieces to a map of all the pieces in the initial board state.

--- include/checker_board.hpp
+++ include/checker_board.hpp
@@ -7,16 +7,60 @@
  * Purpose: A board and functions for playing checkers.
  */
 
+#include <unordered_map>
+#include <string>
 #include <ostream>
+#include "game_piece.hpp"
+
 
 class Checker_Board
 {
 public:
+	Checker_Board()
+	{
+		int board_spaces = 32;
+		for (char board_column = 'a'; board_column < 'i'; ++board_column) {
+			for (int board_row = 0; board_row < 8; ++board_row) {
+				if (board_column == 'a' || board_column == 'c') {
+					if (board_row%2 == 0) {
+						std::string location = board_column + std::to_string(board_row);
+						_board_pieces[location] = Game_Piece('r', "pawn", std::make_pair(board_column, board_row));
+					}
+				} else if (board_column == 'c') {
+					if (board_row%2 == 1) {
+						std::string location = board_column + std::to_string(board_row);
+						_board_pieces[location] = Game_Piece('r', "pawn", std::make_pair(board_column, board_row));
+					}
+				} else if (board_column == 'f' || board_column == 'h') {
+					if (board_row%2 == 1) {
+						std::string location = board_column + std::to_string(board_row);
+						_board_pieces[location] = Game_Piece('b', "pawn", std::make_pair(board_column, board_row));
+					}
+				} else if (board_column == 'g') {
+					if (board_row%2 == 0) {
+						std::string location = board_column + std::to_string(board_row);
+						_board_pieces[location] = Game_Piece('b', "pawn", std::make_pair(board_column, board_row));
+					}
+				}
+			}
+		}
+	}
+
+
 	void print_board(std::ostream & output_stream)
 	{
 		output_stream << _board;
 	}
+	
+
+	std::vector<coordinate> get_moves(coordinate board_coordinate)
+	{
+		// TODO
+		std::vector<coordinate> moves;
+		return moves;
+	}
 private:
+	std::unordered_map<std::string, Game_Piece> _board_pieces;
 	std::string _board = "_w_w_w_w\n"
 				"w_w_w_w_\n"
 				"_w_w_w_w\n"
