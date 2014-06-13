e251ece73bf38b01b645ba1688a50cd74f4d56f6
Bucky Frost <frozenouthere@gmail.com>
2014-06-13 02:15:25 -0800

    Modification of minimax.hpp
    Have a prototype of the minimax algorithm for the evaluations of the
    board states. There is no recursion in the search at this point.

--- include/minimax.hpp
+++ include/minimax.hpp
@@ -7,28 +7,101 @@
 #include "neural_network.hpp"
 #include "checker_board.hpp"
 #include <queue>
+#include <limits>
 
-double minimax(Neural_Network & white_player, Neural_Network & red_player, Checker_Board & board, int ply)
+typedef std::pair<coordinate, std::vector<coordinate> > piece_moves;
+
+Checker_Board min(Neural_Network & player, Checker_Board board, std::vector<piece_moves> moves);
+Checker_Board max(Neural_Network & player, Checker_Board board, std::vector<piece_moves> moves);
+Checker_Board minimax(Neural_Network & player, Checker_Board & board, int depth, char player_color);
+
+
+// Maybe have the order of the network be the determining factor for maximizing
+Checker_Board minimax(Neural_Network & player, Checker_Board & board, int depth, char player_color)
 {
-	// TODO
-	std::priority_queue<std::vector<coordinate> > pieces_moves;
+	if (depth == 0) {
+		return board;
+	}
 
+	std::vector<piece_moves> red_moves;
+	std::vector<piece_moves> white_moves;
+
+	//double best_value = std::numeric_limits<double>::max();
+
+	// Get the moves of current board
 	for (int row = 0; row < 8; ++row) {
 		for (int col = 0; col < 8; ++col) {
 			auto location = std::make_pair(row, col);
-			if (ply % 2 == 0) {
-				if (board.is_red()) {
-					auto move = board.get_move();
-					Checker_Board evaluation_board = board;
-					evaluation_board.move_piece(location, move);
-				} 
-			else {
-				if (board.is_white()) {
-					piece_moves.emplace(board.get_move());
-				}
+			if (board.is_red(location)) {
+				red_moves.emplace_back(std::make_pair(location, board.get_moves(location)));
+			} else if (board.is_white(location)) {
+				white_moves.emplace_back(std::make_pair(location, board.get_moves(location)));
 			}
 		}
 	}
 
+	if (depth % 2 == 0) { // Player's turn, max
+		switch (player_color) {
+		case 'r':
+			return max(player, board, red_moves);
+			break;
+		case 'w':
+			return max(player, board, white_moves);
+			break;
+		default:
+			break;
+		}
+	} else { // Oponent's turn, min
+		switch (player_color) {
+		case 'r':
+			return min(player, board, red_moves);
+			break;
+		case 'w':
+			return min(player, board, white_moves);
+			break;
+		default:
+			break;
+		}
+	}
+}
 
+Checker_Board min(Neural_Network & player, Checker_Board board, std::vector<piece_moves> moves) 
+{
+	double best_value = std::numeric_limits<double>::max();
+	auto eval_board = board;
+	Checker_Board best_state;
+
+	for (auto move : moves) {
+		for (int ii = 0; ii < move.second.size(); ++ii) {
+			eval_board.move_piece(move.first, move.second[ii]);
+			auto board_state = eval_board.get_state();
+			if (player.network_evaluate(board_state) < best_value) {
+				best_value = player.network_evaluate(board_state);
+				best_state = eval_board;
+			}
+		}
+		eval_board = board;
+	}
+	return best_state;
+}
+
+
+Checker_Board max(Neural_Network & player, Checker_Board board, std::vector<piece_moves> moves)
+{
+	double best_value = std::numeric_limits<double>::lowest();
+	auto eval_board = board;
+	Checker_Board best_state;
+
+	for (auto move : moves) {
+		for (int ii = 0; ii < move.second.size(); ++ii) {
+			eval_board.move_piece(move.first, move.second[ii]);
+			auto board_state = eval_board.get_state();
+			if (player.network_evaluate(board_state) > best_value) {
+				best_value = player.network_evaluate(board_state);
+				best_state = eval_board;
+			}
+		}
+		eval_board = board;
+	}
+	return best_state;
 }
