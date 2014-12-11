5dd8bdc4da22df796dfa2e6fd19c5b19594ff5d3
Bucky Frost <frozenouthere@gmail.com>
2014-12-11 05:35:33 +0000

    Cleaned up the main and created some functions for timing and testing.
    Created two functions
    * void create_NN() -> will create a random NN and serialize to file.
    * void timing() -> moving the timing code to a separate func than main.
    
    Now have a globally defined vector for the standard network.

--- src/main.cu
+++ src/main.cu
@@ -13,63 +13,44 @@
 using std::cout;
 using std::endl;
 
+std::vector<int> standard_network = {32, 40, 10, 1};
+
+void create_NN();
+void timing();
+
 int main(int argc, char* argv[])
 {
-	std::vector<int> standard_network = {32, 40, 10, 1};
-	std::vector<float> board_state;
+	return 0;
+}
 
-	Neural_Network red_player(standard_network);
-	Neural_Network white_player(standard_network);
+void create_NN()
+{
+        G_Neural_Network * base_case_network = new G_Neural_Network(standard_network);
 
-	Checker_Board board;
+	std::ofstream ofs("test_network_save.txt");
 
-	board_state = board.get_state();
+	boost::archive::text_oarchive oa(ofs);
 
-	for (int row = 0; row < 8; ++row)
-		for (int col = 0; col < 8; ++col)
-			std::make_pair(row, col);
-        // Timing
-        auto start_cpu = std::chrono::high_resolution_clock::now();
-	red_player.network_evaluate(board_state);
-        auto end_cpu = std::chrono::high_resolution_clock::now();
-        cout << "Red Player: " <<
-        std::chrono::duration<double, std::nano> (end_cpu - start_cpu).count() << "ns" << endl;
-        // end timing
+	oa << base_case_network;
+	ofs.close();
 
-        // GPU Timing
-        auto start_gpu = std::chrono::high_resolution_clock::now();
+}
 
-        // Allocate Space shared between cpu and gpu
+void timing()
+{
         int n=32*40*8; // size of network... total number of floats
         G_Neural_Network * whitey = new G_Neural_Network(standard_network);
 
         // Run gpu stuff on shared space
         int nBlocks = 1;    // GPU thread blocks to run
         int blockDim = n;   // threads per block, should be 256 for best performance
-        evaluate<<<nBlocks, blockDim>>>(whitey);    // evaluate on gpu
-
-        cudaDeviceSynchronize();    // wait to finish evaluation
-        //white_player.network_evaluate(board_state);
-
-        auto end_gpu = std::chrono::high_resolution_clock::now();
-        cout << "White Player: " <<
-        std::chrono::duration<double, std::nano> (end_gpu - start_gpu).count() << "ns" << endl;
-        // end GPU timing
-
-
-        /*
-	board.print_board(std::cout);
-
-	while (1) {
-		board = minimax(red_player, board, 6, 'r');
-
-		std::cout << std::endl;
-		board.print_board(std::cout);
-
-		board = minimax(white_player, board, 6, 'w');
-		std::cout << std::endl;
-		board.print_board(std::cout);
+	for (int ii = 0; ii < 100; ++ii) {
+        	auto start_gpu = std::chrono::high_resolution_clock::now();
+		evaluate<<<nBlocks, blockDim>>>(whitey);    // evaluate on gpu
+		cudaDeviceSynchronize();    // wait to finish evaluation
+        	auto end_gpu = std::chrono::high_resolution_clock::now();
+        	cout << std::chrono::duration<double, std::nano> (end_gpu - start_gpu).count() << endl;
 	}
-        */
-	return 0;
+
+
 }
