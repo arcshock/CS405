399d6cbcd43dc549d70b4553cd26f07dd4da262d
Bucky Frost <frozenouthere@gmail.com>
2014-12-23 02:54:24 -0900

    Removed sigmoid functionality, fixed compiler warnings.

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -30,13 +30,7 @@ private:
 		ar & _network;
 	}
 
-	double sigmoid(double input)
-	{
-		return input/(1.0 + abs(input));
-	}
-
 	network _network;
-
 public:
 	Neural_Network(std::vector<int> network_specifications) : Player(' ', "unknown")
 	{
@@ -54,7 +48,7 @@ public:
 		double evaluation_value = 0.0;
 
 		int network_input_size = _network[0].size();
-		int input_layer = 0; // Remove this value and integrate the for loops for simplification
+		unsigned int input_layer = 0; // Remove this value and integrate the for loops for simplification
 
 		try {
 			for (int inputIdx = 0; inputIdx < network_input_size; ++inputIdx) {
@@ -65,10 +59,10 @@ public:
 		}
 
 		try { 
-			for (int network_layer = input_layer + 1; network_layer < _network.size(); ++network_layer) {
-				for (int layer_column = 0; layer_column < _network[network_layer].size(); ++layer_column) {
-					for (int ii = 0; ii < _network[network_layer - 1].size(); ++ii) {
-						_network[network_layer][layer_column]._input += sigmoid(_network[network_layer - 1][ii].node_value());
+			for (unsigned int network_layer = input_layer + 1; network_layer < _network.size(); ++network_layer) {
+				for (unsigned int layer_column = 0; layer_column < _network[network_layer].size(); ++layer_column) {
+					for (unsigned int ii = 0; ii < _network[network_layer - 1].size(); ++ii) {
+						_network[network_layer][layer_column]._input += (_network[network_layer - 1][ii].node_value());
                                         }
 
 				}
@@ -79,8 +73,9 @@ public:
 
 		auto network_output = _network.back();
 		evaluation_value = _network.back().back().node_value();
-		return sigmoid(evaluation_value);
+		return evaluation_value;
 	}
+
 
 	bool operator==(const Neural_Network & other) const
 	{
