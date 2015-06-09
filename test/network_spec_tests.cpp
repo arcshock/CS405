3751884e0cb06b3378c9bd6757730501b52acd81
Bucky Frost <frozenouthere@gmail.com>
2015-06-09 11:31:59 -0800

    Testing for correct network topography from string description.

--- test/network_spec_tests.cpp
+++ test/network_spec_tests.cpp
@@ -5,28 +5,24 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-#include "catch_main.cpp"
+#include "catch.hpp"
 #include "network_spec.hpp"
 
-//const std::string standard_network_topography("32 40 10 1");
-//std::vector<double> zeroed_input(32, 0.0);
+const std::string standard_network_topography("32 40 10 1");
 
-TEST_CASE ("Network Spec") {
-
+TEST_CASE ("Network Specification") {
 	SECTION ("construction from string") {
-//		Network_Spec network_spec_ctor_test(standard_network_topography);	
+		Network_Spec network_spec_ctor_test(standard_network_topography);
 
+		std::istringstream buffer(standard_network_topography);
+		unsigned int tmp = 0;
 
-//		std::istringstream buffer(standard_network_topography);
-//		unsigned int tmp = 0;
-
-
-//		for (auto it = network_spec_ctor_test.begin(); it != network_spec_ctor_test.end(); ++it) {
-//			if (buffer.good()) {
-//				buffer >> tmp;
-//			}
-//			REQUIRE( *it == tmp );
-//		}
+		for (auto it = network_spec_ctor_test.begin(); it != network_spec_ctor_test.end(); ++it) {
+			if (buffer.good()) {
+				buffer >> tmp;
+			}
+			REQUIRE( *it == tmp );
+		}
 	}
 }
 #endif /*NETWORK_SPEC_TEST*/
