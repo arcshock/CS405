29d5a1a4cd20ee02529848ef021f8450fa18af79
Bucky Frost <frozenouthere@gmail.com>
2015-06-15 04:04:00 -0800

    Added test for the evaluation function of the NN.

--- test/network_spec_tests.cpp
+++ test/network_spec_tests.cpp
@@ -5,11 +5,13 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <vector>
 #include "catch.hpp"
 #include "network_spec.hpp"
 #include "neural_network.hpp"
 
 const std::string standard_network_topography("32 40 10 1");
+std::vector<double> eval_data(0, 32);
 
 TEST_CASE ("Network Specification") {
 	SECTION ("construction from String") {
@@ -31,6 +33,12 @@ TEST_CASE ("Neural Network") {
 	SECTION ("Construction from Specification") {
 		Network_Spec net_spec(standard_network_topography);
 		Neural_Network spec_constructed(net_spec);
+	}
+
+	SECTION ("Evaluation Function") {
+		Network_Spec net_spec(standard_network_topography);
+		Neural_Network spec_constructed(net_spec);
+		REQUIRE ( spec_constructed.start(eval_data.begin(), eval_data.end()) != 0 );
 	}
 }
 #endif /*NETWORK_SPEC_TEST*/
